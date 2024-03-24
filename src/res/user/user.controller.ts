85c68608caac586551a2f51a857e1a4dd1733785
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-03-25 01:34:51 +0900

    Feat: 회원가입 API작성

--- src/res/user/user.controller.ts
+++ src/res/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
-@Controller("/user")
+@Controller("user")
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
-    @Get("/main")
+    @Get("main")
     async getMainPage() {
         return this.userService.getMainPage();
+    }
+
+    @Post("register")
+    async register(@Body() body) {
+        const email = body?.email;
+        const password = body?.password;
+
+        return this.userService.register(email, password);
     }
 
 }
