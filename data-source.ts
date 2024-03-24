2a6254bf896eb8e47548b5db41b2cbc439a493ff
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-03-25 00:42:55 +0900

    Refactor: cross-env설정

--- data-source.ts
+++ data-source.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: `.env.dev` });
 
 export const dataSource = new DataSource({
     type: "mysql",
@@ -10,7 +10,7 @@ export const dataSource = new DataSource({
     port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
-    password: process.env.DB_PASSZORD,
+    password: process.env.DB_PASSWORD,
     entities: [
         path.join(__dirname, "src/entities/**/*.entity.ts"),
         path.join(__dirname, "dist/entities/**/*.entity.js"),
