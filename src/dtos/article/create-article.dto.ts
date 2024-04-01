b83f31a096bfa8726accbb0c83262c3263d2723c
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-04-01 20:40:50 +0900

    Feat: CreateArticleDto 작성

---
+++ src/dtos/article/create-article.dto.ts
@@ -0,0 +1,9 @@
+import { PickType } from "@nestjs/swagger";
+import { ArticleEntity } from "src/entities/article.entity";
+
+
+export class CreateArticleDto extends PickType(
+    ArticleEntity, [
+    "title",
+    "content",
+]) { }
